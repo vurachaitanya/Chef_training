622a0718d1063f1809d3a1bdd5fb797c85cc9adc
vurachaitanya <vurachaitanya@gmail.com>
2018-07-24 02:16:16 -0700

    updated

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+package "apache2" do
+    action :install
+end
+
+service "apache2" do
+    action [ :enable, :start ]
+end
