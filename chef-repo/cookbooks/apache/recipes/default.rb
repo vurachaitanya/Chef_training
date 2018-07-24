6054e31ff6482523d64eb00671b75e983c13d501
vurachaitanya <vurachaitanya@gmail.com>
2018-07-24 04:59:40 -0700

    updated

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -11,8 +11,12 @@ service "apache2" do
     action [ :enable, :start ]
 end
 
+#cookbook_file "/var/www/html/index.html" do
+#    source "index.html"
+#    mode "0644"
+#end
+node.default["apache2"]["indexfile"] = "index2.html"
 cookbook_file "/var/www/html/index.html" do
-    source "index.html"
+    #source node ["apache"]["indexfile"]
     mode "0644"
 end
-
--- chef-repo/cookbooks/apache1/recipes/default.rb
+++ chef-repo/cookbooks/apache1/recipes/default.rb
@@ -11,6 +11,7 @@ service "apache2" do
     action [ :enable, :start ]
 end
 
+node.default["apache"]["indexfile"] = "index2.html"
 cookbook_file "/var/www/html/index.html" do
     source node ["apache1"]["indexfile"]
     mode "0644"
