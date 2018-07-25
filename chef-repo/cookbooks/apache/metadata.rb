2b96f28582ec6004982ce560a1fd9112ea070f04
vurachaitanya <vurachaitanya@gmail.com>
2018-07-25 00:27:17 -0700

    updated

--- chef-repo/cookbooks/apache/metadata.rb
+++ chef-repo/cookbooks/apache/metadata.rb
@@ -6,6 +6,7 @@ description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
 version '0.3.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
+depends "motd"
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
